edd4d6821596f3e1369ad7f135b19117d4af7947
Death_In_Rome <Death_In_Rome>
2023-10-15 19:52:46 +0300

    Доработан класс Plugins

--- lib/Plugin/PluginFactory/Implementation/Plugins.cs
+++ lib/Plugin/PluginFactory/Implementation/Plugins.cs
@@ -5,7 +5,7 @@ namespace ds.test.impl
     /// <summary>
     /// Контейнер с плагинами
     /// </summary>
-    public  class Plugins : IPluginFactory
+    public class Plugins : IPluginFactory
     {
         //Коллекия плагинов
         private static readonly List<IPlugin> _plugins = new();
@@ -27,9 +27,6 @@ namespace ds.test.impl
         /// </summary>
         public IPlugin GetPlugin(string namePlugin)
         {
-            if(_plugins.Count<1)
-                throw new ArgumentNullException($"Коллекция с плагинами пуста.");
-
             IPlugin? plugin = _plugins
                 .SingleOrDefault(plugin => plugin.PluginName == namePlugin);
 
@@ -42,7 +39,7 @@ namespace ds.test.impl
         public void RegisterPlugin(IPlugin plugin)
         {
             if (plugin == null)
-                throw new ArgumentNullException(nameof(plugin));
+                throw new ArgumentNullException($"Пустой аргумент") ;
 
             _plugins.Add(plugin);
         }
